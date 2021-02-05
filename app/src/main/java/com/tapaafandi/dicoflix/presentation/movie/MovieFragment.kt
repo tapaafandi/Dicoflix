5807b6823caeb7230cd9d36a70b4f8bbfe1ca468
Mustafa Afandi <tapaafandi@gmail.com>
2021-02-05 16:54:47 +0700

    add splash screen

--- app/src/main/java/com/tapaafandi/dicoflix/presentation/movie/MovieFragment.kt
+++ app/src/main/java/com/tapaafandi/dicoflix/presentation/movie/MovieFragment.kt
@@ -2,9 +2,7 @@ package com.tapaafandi.dicoflix.presentation.movie
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tapaafandi.dicoflix.R
--- app/src/main/java/com/tapaafandi/dicoflix/presentation/tv_show/TvShowFragment.kt
+++ app/src/main/java/com/tapaafandi/dicoflix/presentation/tv_show/TvShowFragment.kt
@@ -2,16 +2,12 @@ package com.tapaafandi.dicoflix.presentation.tv_show
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tapaafandi.dicoflix.R
-import com.tapaafandi.dicoflix.adapter.MovieAdapter
 import com.tapaafandi.dicoflix.adapter.TvShowAdapter
 import com.tapaafandi.dicoflix.databinding.FragmentTvShowBinding
-import com.tapaafandi.dicoflix.presentation.movie.MovieViewModel
 
 class TvShowFragment : Fragment(R.layout.fragment_tv_show) {
 
